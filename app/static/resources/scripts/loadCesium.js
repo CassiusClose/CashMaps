968ebc26adb65e5a183db6fce06cb9243813ff59
CassiusClose <CassiusClose@gmailcom>
2019-07-25 17:21:01 -0400

    Added cesium page

---
+++ app/static/resources/scripts/loadCesium.js
@@ -0,0 +1,5 @@
+//Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYmJlYzNlYS0yN2UwLTRmMGMtOWMyMi1iYjMwMzQzMzgzYjYiLCJpZCI6ODI4Nywic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTY3OTM2OX0.EGK5EcrEjcL-Wi6CN_iPtzHKsxwHOn_vhXep3qjbfQU';
+
+var viewer = new Cesium.Viewer('cesiumContainer');
+
+var scene = viewer.scene
