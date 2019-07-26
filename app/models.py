4d44e5d15c9784b72f6fcd6f0ab6ba6ea51c7658
CassiusClose <CassiusClose@gmailcom>
2019-07-26 03:28:12 -0400

    removed redundant segment model

--- app/models.py
+++ app/models.py
@@ -4,20 +4,13 @@ from app import db
 class Track(db.Model):
     database_id = db.Column(db.Integer, primary_key=True)
     track_id = db.Column(db.Integer)
-    segments = db.relationship('TrackSegment', backref='track', lazy='dynamic')
-    
-class TrackSegment(db.Model):
-    __table_args__ = (db.UniqueConstraint('segment_id', 'track_id'),)
-    database_id = db.Column(db.Integer, primary_key=True)
-    segment_id = db.Column(db.Integer)
-    points = db.relationship('TrackPoint', backref='segment', lazy='dynamic')
-    track_id = db.Column(db.Integer, db.ForeignKey('track.track_id'))
+    points = db.relationship('TrackPoint', backref='track', lazy='dynamic')
 
 class TrackPoint(db.Model):
-    __table_args__ = (db.UniqueConstraint('point_id', 'segment_id', 'segment.track_id'),)
+    __table_args__ = (db.UniqueConstraint('point_id', 'track_id'),)
     database_id = db.Column(db.Integer, primary_key=True)
     point_id = db.Column(db.Integer)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     timestamp = db.Column(db.DateTime, index=True)
-    segment_id = db.Column(db.Integer, db.ForeignKey('track_segment.segment_id'))
+    track_id = db.Column(db.Integer, db.ForeignKey('track.track_id'))
