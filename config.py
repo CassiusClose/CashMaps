233af6176f142eda694085d2b8dadc2c113ca5d0
CassiusClose <CassiusClose@gmailcom>
2019-07-25 23:22:31 -0400

    Set up database

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cash_MapS_Key_Babyy'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join('static/resources/database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
