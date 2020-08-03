5993eb95843c448bba83aeb14cd05e0c2c97896b
Cassius Close <CassiusClose@gmail.com>
2020-08-03 16:45:36 -0400

    Consolidated parser task testing and parser socket testing into one set of tests

--- server/cashmaps/tests/parsing/test_homeport_parser.py
+++ server/cashmaps/tests/parsing/test_homeport_parser.py
@@ -118,17 +118,30 @@ class TestHomeportParserAsTask:
     tests that callbacks from that job are called, which do cleanup tasks like deleting
     the parsed file from the temp directory. This class tests a whole bunch of different
     cases where parsing would fail, and tests that the error callback is called as well.
+
+    It also tests that socket messages are sent properly, which currently will only happen
+    when the parse is done as a RQ Job.
     """
 
-    def test_parse_task_fine(self, app, database, worker):
+    def test_parse_task_fine(self, app, socketio_client, database, worker):
         """
         Tests that a successful parse creates the proper database objects, and removes
         the parsed file from the temp directory.
+
+        Also tests that the proper socket messages were sent: one signaling the start
+        of the parse, one signaling the successful completion of the parse, and many
+        updating the job's progress in the middle.
         """
-        filepath = get_testfile_path('standard.txt')
+        # Open the file to be parsed
+        parse_filename = 'standard.txt'
+        filepath = get_testfile_path(parse_filename)
         f = open(filepath)
         file = FileStorage(stream=f) #simulate post request file object
 
+        # Connect to the socket
+        socketio_client.connect(namespace='/parsers')
+
+        # Do the parse
         job = start_parse(file, weird=True)
         worker.work(burst=True)
 
@@ -140,115 +153,7 @@ class TestHomeportParserAsTask:
         assert not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER_TEMP'],'standard.txt'))
         assert os.path.exists(filepath)
 
-
-    def _test_parse_task_error(self, app, database, worker, filename):
-        """
-        A helper function. Called by the following tests.
-        Tests that parsing the given file will result in error, i.e. not saving any
-        data to the database, but still removing the parsed file from the temp directory.
-        """
-        # Load the file to be parsed in a file object
-        filepath = get_testfile_path(filename)
-        f = open(filepath)
-        file = FileStorage(stream=f) #simulate post request file object
-
-        job = start_parse(file, weird=True)
-        worker.work(burst=True)
-
-        # No database objects should have been created
-        assert Track.query.count() == 0
-        assert TrackPoint.query.count() == 0
-
-        # The temp file should be gone, and it shouldn't change the original one.
-        assert not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER_TEMP'],filename))
-        assert os.path.exists(filepath)
-
-
-    def test_parse_task_error_file_random_text(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file is just random sentences.
-        """
-        self._test_parse_task_error(app, database, worker, 'random_text.txt')
-
-
-    def test_parse_task_error_empty_file(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file contains no text.
-        """
-        self._test_parse_task_error(app, database, worker, 'empty_file.txt')
-
-
-    def test_parse_task_error_bad_lat_long(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file has an incorrectly
-        formatted latitude or longitude point.
-        """
-        self._test_parse_task_error(app, database, worker, 'bad_lat_long.txt')
-
-
-    def test_parse_task_error_bad_track_id(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file has a track id that
-        is negative.
-        """
-        self._test_parse_task_error(app, database, worker, 'bad_track_id.txt')
-
-
-    def test_parse_task_error_bad_point_track_id(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file has a point that
-        has a track id that doesn't match any track listed in the file.
-        """
-        self._test_parse_task_error(app, database, worker, 'bad_point_track_id.txt')
-
-
-    def test_parse_task_error_bad_timestamp(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file has a timestamp that's
-        formatted incorrectly.
-        """
-        self._test_parse_task_error(app, database, worker, 'bad_timestamp.txt')
-
-
-    def test_parse_task_error_bad_tabs(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file doesn't have tabs between
-        its pieces of data.
-        """
-        self._test_parse_task_error(app, database, worker, 'bad_tabs.txt')
-
-
-    def test_parse_task_error_missing_sections(self, app, database, worker):
-        """
-        Tests that the parser fails correctly when the file doesn't have all the
-        sections that a homeport parser is supposed to have.
-        """
-        self._test_parse_task_error(app, database, worker, 'missing_sections.txt')
-
-
-
-
-class TestHomeportParserSockets:
-    """
-    Tests that the parser send updates about its progress over the parsers' socket
-    namespace. It also tests the start_parse function, which puts the parse as a job
-    in the Redis Queue, because that function and its callbacks also send socket messages.
-    """
-
-    def test_parse_socket_success(self, app, socketio_client, database, worker):
-        """
-        Tests that a succesful parse will send socket messages indicating its beginning,
-        end, and progress in between.
-        """
-        filepath = get_testfile_path('standard.txt')
-        f = open(filepath)
-        file = FileStorage(stream=f) #simulate post request file object
-
-        socketio_client.connect(namespace='/parsers')
-
-        job = start_parse(file, weird=True)
-        worker.work(burst=True)
-
+        # Get all socket messages that were sent
         received_messages = socketio_client.get_received(namespace='/parsers')
 
         # Ensure the message from the parser beginning was sent
@@ -270,30 +175,44 @@ class TestHomeportParserSockets:
             assert m['args'][0]['progress'] == i-1
 
 
-    def test_parse_socket_failure(self, app, socketio_client, database, worker):
+    def _test_parse_task_error(self, app, socketio_client, database, worker, filename):
         """
-        Tests that a failed parse will send socket messages indicating its beginning,
-        progress up to the failed point, and failure.
-        """
-        parse_filename = 'bad_lat_long.txt'
-        filepath = get_testfile_path(parse_filename)
-        file = FileStorage(stream=open(filepath))
+        A helper function. Called by the following tests.
+        Tests that parsing the given file will result in error, i.e. not saving any
+        data to the database, but still removing the parsed file from the temp directory.
 
+        Also tests that the proper socket messages were sent: One signaling the beginning
+        of the parsing, many updates of the job's progress, and one signaling that the
+        parse failed.
+        """
+        # Load the file to be parsed in a file object
+        filepath = get_testfile_path(filename)
+        f = open(filepath)
+        file = FileStorage(stream=f) #simulate post request file object
+
+        # Connect to the socket
         socketio_client.connect(namespace='/parsers')
 
+        # Do the parse
         job = start_parse(file, weird=True)
         worker.work(burst=True)
 
-        received_messages = socketio_client.get_received(namespace='/parsers')
+        # No database objects should have been created
+        assert Track.query.count() == 0
+        assert TrackPoint.query.count() == 0
 
-        # The 71st point is the one that the parser fails on, so make sure that
-        # the progress messages are sent (0-71), plus one for start and one for failure
-        assert len(received_messages) == 74
+        # The temp file should be gone, and it shouldn't change the original one.
+        assert not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER_TEMP'],filename))
+        assert os.path.exists(filepath)
+        
+
+        # Get all socket messages that were sent
+        received_messages = socketio_client.get_received(namespace='/parsers')
 
         # Ensure the parser start message was sent
         assert received_messages[0]['name'] == 'parser_start'
         assert received_messages[0]['args'][0]['job_id'] == job.id
-        assert received_messages[0]['args'][0]['filename'] == parse_filename
+        assert received_messages[0]['args'][0]['filename'] == filename 
 
         # Ensure the parser failure message was sent
         assert received_messages[-1]['name'] == 'parser_error'
@@ -304,8 +223,68 @@ class TestHomeportParserSockets:
             m = received_messages[i]
             assert m['name'] == 'parser_update'
             assert m['args'][0]['job_id'] == job.id
-            assert m['args'][0]['filename'] == parse_filename
+            assert m['args'][0]['filename'] == filename
             assert m['args'][0]['max_progress'] == 100
             assert m['args'][0]['progress'] == i-1
 
 
+    def test_parse_task_error_file_random_text(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file is just random sentences.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'random_text.txt')
+
+
+    def test_parse_task_error_empty_file(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file contains no text.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'empty_file.txt')
+
+
+    def test_parse_task_error_bad_lat_long(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file has an incorrectly
+        formatted latitude or longitude point.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'bad_lat_long.txt')
+
+
+    def test_parse_task_error_bad_track_id(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file has a track id that
+        is negative.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'bad_track_id.txt')
+
+
+    def test_parse_task_error_bad_point_track_id(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file has a point that
+        has a track id that doesn't match any track listed in the file.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'bad_point_track_id.txt')
+
+
+    def test_parse_task_error_bad_timestamp(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file has a timestamp that's
+        formatted incorrectly.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'bad_timestamp.txt')
+
+
+    def test_parse_task_error_bad_tabs(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file doesn't have tabs between
+        its pieces of data.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'bad_tabs.txt')
+
+
+    def test_parse_task_error_missing_sections(self, app, socketio_client, database, worker):
+        """
+        Tests that the parser fails correctly when the file doesn't have all the
+        sections that a homeport parser is supposed to have.
+        """
+        self._test_parse_task_error(app, socketio_client, database, worker, 'missing_sections.txt')
