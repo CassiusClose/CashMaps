8168fe20ef1bbedc8b1e7697103632d2c4970031
Cassius Close <CassiusClose@gmail.com>
2020-08-04 21:28:54 -0400

    Removed reduncancy in Flask config by adding a BaseConfig class that all config classes subclass

--- server/config.py
+++ server/config.py
@@ -1,7 +1,6 @@
 import os
 
-
-class DevConfig():
+class BaseConfig():
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     # Set unbuffered output, i.e., output is sent immediately to STDOUT instead
@@ -9,46 +8,37 @@ class DevConfig():
     os.environ['PYTHONUNBUFFERED'] = "TRUE"
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cash_MapS_Key_Babyy'
-    
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir + '/cashmaps/static/resources/database.db')
 
     #Turns off notifications when the database is updated
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
+    # Redis server for running background tasks
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
 
-
+    # Where are uploaded files stored
     UPLOAD_FOLDER_TEMP=os.path.join(basedir +  "/cashmaps/static/resources/temp/")
     UPLOAD_FOLDER_PHOTOS=os.path.join(basedir + "/cashmaps/static/photos/")
-    TASK_TYPE_PARSE='parse'
+
+    # Socket namespaces
+    TASK_TYPE_PARSE = 'parse'
 
 
-class TestConfig():
-    basedir = os.path.abspath(os.path.dirname(__file__))
+class DevConfig(BaseConfig):
 
     # Set unbuffered output, i.e., output is sent immediately to STDOUT instead
     # of being buffered
     os.environ['PYTHONUNBUFFERED'] = "TRUE"
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cash_MapS_Key_Babyy'
-   
+    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BaseConfig.basedir + '/cashmaps/static/resources/database.db')
 
+
+class TestConfig(BaseConfig):
     TESTING = True
     WTF_CSRF_ENABLED = False
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir + '/cashmaps/static/resources/test_db.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BaseConfig.basedir + '/cashmaps/static/resources/test_db.db')
 
-    #Turns off notifications when the database is updated
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
-
-
-    UPLOAD_FOLDER_TEMP=os.path.join(basedir +  "/cashmaps/static/resources/temp/")
-    UPLOAD_FOLDER_PHOTOS=os.path.join(basedir + "/cashmaps/static/photos/")
-    TASK_TYPE_PARSE='parse'
 
     # The port of the Pytest-Flask live server for Browser testing
     LIVESERVER_PORT = 5000
